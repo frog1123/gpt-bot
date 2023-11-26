6d305b0b1cb60d27c717097c958bcb0004343d5c
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-26 14:07:45 -0600

    +

--- src/slash-commands/chat.ts
+++ src/slash-commands/chat.ts
@@ -1,8 +1,16 @@
 import { SlashCommandBuilder } from "discord.js";
+import { openai } from "../openai";
 
 export default {
   data: new SlashCommandBuilder().setName("chat").setDescription("this runs the chat command"),
   execute: async (interaction: any) => {
-    await interaction.reply("you used the chat command");
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo"
+    });
+
+    console.log(chatCompletion.choices);
+
+    await interaction.reply(chatCompletion.choices[0].message);
   }
 };
