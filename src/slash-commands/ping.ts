1b6c7f68ee91d66bc71e46c303beab4f72d3b339
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-26 13:31:38 -0600

    Update ping.ts

--- src/slash-commands/ping.ts
+++ src/slash-commands/ping.ts
@@ -3,6 +3,12 @@ import { SlashCommandBuilder } from "discord.js";
 export default {
   data: new SlashCommandBuilder().setName("ping").setDescription("this runs the ping command"),
   execute: async (interaction: any) => {
-    await interaction.reply("you used a slash command");
+    const startTime = Date.now();
+    const reply = await interaction.reply("Pinging...");
+
+    const endTime = Date.now();
+    const responseTime = endTime - startTime;
+
+    await reply.edit(`Pong! Response time: \`${responseTime}ms\``);
   }
 };
