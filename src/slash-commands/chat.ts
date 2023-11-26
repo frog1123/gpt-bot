86f8cb4b8e9b3211afda1e4c762e6c2da476ca7e
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-26 14:36:48 -0600

    Update chat.ts

--- src/slash-commands/chat.ts
+++ src/slash-commands/chat.ts
@@ -7,24 +7,31 @@ export default {
     .setDescription("this runs the chat command")
     .addStringOption(option => option.setName("message").setDescription("Ask bot-gpt...").setRequired(true)),
   execute: async (interaction: any) => {
-    const message = interaction.options.getString("message") ?? "";
-    console.log("msg", message);
+    try {
+      const message = interaction.options.getString("message") || "";
 
-    if (message.length === 0) {
-      await interaction.reply("Message is required");
-      return;
+      if (message.length === 0) {
+        await interaction.reply("Message is required");
+        return;
+      }
+
+      if (message.length >= 100) {
+        await interaction.reply("Message must be under 100 characters");
+        return;
+      }
+
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: message }],
+        model: "gpt-3.5-turbo"
+      });
+
+      console.log(chatCompletion.choices);
+
+      const replyContent = chatCompletion.choices[0]?.message?.content;
+      await interaction.reply(replyContent);
+    } catch (err) {
+      console.error("[CHAT]", err);
+      interaction.reply("Something went wrong :/");
     }
-
-    if (message.length >= 100) {
-      await interaction.reply("Message is must be under 100 characters");
-      return;
-    }
-
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: "user", content: message }],
-      model: "gpt-3.5-turbo"
-    });
-
-    await interaction.reply(chatCompletion.choices[0].message.content);
   }
 };
